83b546123d17a583d6a1dd76875f2375b341a608
ConorM2710 <conormoyles@outlook.com>
2022-08-28 14:13:15 +0000

    Added a large section of the javascript using the lessons on codeinstitue as guidelines alongside some online resources for help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 const cards = document.querySelectorAll('.game-card');
 
+let hasCardFlipped = false;
+let firstCard, secondCard;
+
 function turnCard(){
-    this.classList.toggle("flip")
+    this.classList.add("flip")
+
+
+if (!hasCardFlipped) {
+    hasCardFlipped = true;
+    firstCard = this;
+    } else {
+        hasCardFlipped = false;
+        secondCard = this;
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener('click', turnCard);
+        secondCard.removeEventListener('click', turnCard);
+        } else {
+            setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            }, 1500);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', turnCard));
