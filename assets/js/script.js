386a55eff05264ae9e709b47dcabf3c44bdf6ccf
ConorM2710 <conormoyles@outlook.com>
2022-08-28 15:08:34 +0000

    Add the styling, heading and win calculator to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 const cards = document.querySelectorAll('.game-card');
 
 let hasCardFlipped = false;
+let holdBoard = false
 let firstCard, secondCard;
 
 function turnCard(){
+    if (holdBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add("flip")
 
 
 if (!hasCardFlipped) {
     hasCardFlipped = true;
     firstCard = this;
-    } else {
-        hasCardFlipped = false;
-        secondCard = this;
+
+    return;
+}
+
+    secondCard = this;
 
     checkingForMatch();
     }
-}
 
 function checkingForMatch(){
        let isPair = firstCard.dataset.framework === secondCard.dataset.framework;
@@ -27,13 +32,31 @@ function checkingForMatch(){
 function stopCards() {
     firstCard.removeEventListener('click', turnCard);
     secondCard.removeEventListener('click', turnCard);
+
+    resetBoard();
 }
 
 function resetCards() {
+    holdBoard = true;
+ 
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        
+    resetBoard();
         }, 1500);
 }
+
+function resetBoard(){
+    [hasCardFlipped, holdBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function reset() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 28);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', turnCard));
