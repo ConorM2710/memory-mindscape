075cf9e1d696efa6f7f1ad9d2c84222aa0b81ad2
ConorM2710 <conormoyles@outlook.com>
2022-08-29 19:15:44 +0000

    Added styling to the h1, list and timer, timer works but needs to stop at 0 and send alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+alert('Hurry the timer has begun!!')
+
+/* js for the card game */
 const cards = document.querySelectorAll('.game-card');
 
 let hasCardFlipped = false;
@@ -60,3 +63,21 @@ function resetBoard(){
 })();
 
 cards.forEach(card => card.addEventListener('click', turnCard));
+
+/* js for the timer */
+
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+
+    time--;
+}
