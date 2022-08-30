433a62e7e2650f954e29db0670efa9473483a739
ConorM2710 <conormoyles@outlook.com>
2022-08-30 18:34:20 +0000

    Attempt to add a shuffling of the deck function to improve UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,7 @@ cards.forEach(card => card.addEventListener('click', turnCard));
 const startingMinutes = 2;
 let time = startingMinutes * 60;
 
+
 const countdownEl = document.getElementById('countdown');
 
 setInterval(updateCountdown, 1000);
@@ -77,7 +78,10 @@ function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
 
     time--;
-}
+ }
+                     
